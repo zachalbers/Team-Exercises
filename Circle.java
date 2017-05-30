1fff15e5e29743cc7e89ee5b83dfeb0b72a82c07
zachalbers <back2zach2@gmail.com>
2017-05-30 10:01:01 -0600

    chagnge

--- Circle.java
+++ Circle.java
@@ -4,25 +4,27 @@ import java.awt.Rectangle;
 public class Circle {
 	private Point centre;
 	private int radius;
-	
+
 	public Circle(Point topLeft, Point bottomRight){
 		radius = (int)topLeft.distance(bottomRight)/2;
 		centre = new Point(topLeft.getXCoord() + radius, topLeft.getYCoord() + radius);
 	}
-	
+
 	public Circle(Point centre, int radius) {
 		this.centre = centre;
 		this.radius = radius;
 	}
-	
+
 	public void moveDown(int amount) {
 		centre.moveDown(amount);
 	}
-	
+
 	public void draw(Graphics g){
-		g.drawOval(centre.getXCoord() - radius, centre.getYCoord() - radius, 
-				radius * 2, 
+		g.drawOval(centre.getXCoord() - radius, centre.getYCoord() - radius,
+				radius * 2,
 				radius * 2);
-		
+
+
+
 	}
 }
