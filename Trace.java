a23a18786c74c59f0809cf6129ae9c0394d44412
zachalbers <back2zach2@gmail.com>
2017-05-31 10:50:07 -0600

    nothing

--- Trace.java
+++ Trace.java
@@ -4,14 +4,33 @@ public class Trace {
 		Point p = new Point(100,200);
 		Circle c = new Circle(p,50);
 		Square s = new Square(p,75);
-		
+
 		c.moveDown(10);
 		System.out.println(p);
 		p.moveLeft(100);
 		System.out.println(p);
 		s.moveDown(40);
 		System.out.println(p);
-		
+
 	}
 
 }
+
+
+/* Starts by creating a new object 'p' of class point with two 'ints' passed in as parameters;
+		Checks the parameters to see if they are valid (greater than 0);
+		The instance variables of Point 'p' are set:
+				int 'xcoord' = 100;
+				int 'ycoor' = 200;
+Creates a new object 'c' of class Circle, with a reference to 'p' and an 'int' as parameters;
+		The instance variables of Circle 'c' are set:
+				Point 'centre' is passed a reference to the Point 'p';
+				
+Creates a new object 's' of class Circle, with a reference to 'p' and an 'int' as parameters;
+Calls the method moveDown() with an 'int' as a parameter;
+		Checks first to see if the parameter is more than 0;
+		Adds the parameter passed in to the instance variable 'ycoord' of the Point p;
+Prints out a string representation of the object 'p' to the console;
+Calls the method moveLeft() with an 'int' as a parameter;
+		Checks first to see if the parameter is more than 0;
+		Adds the parameter passed in to the instance variable 'xcoord' of the Point p;
